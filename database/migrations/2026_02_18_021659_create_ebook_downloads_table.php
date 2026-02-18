46b5e4a4a7db9d8e32a5a2b1e6e9787e83ecfe65
Nazmi Ramadani <ramadaninazmi47@gmail.com>
2026-02-18 09:19:29 +0700

    feat: add migration for ebook

---
+++ database/migrations/2026_02_18_021659_create_ebook_downloads_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ebook_downloads', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('ebook_id')->constrained()->onDelete('cascade');
+            $table->timestamp('downloaded_at');
+
+            $table->unique(['user_id', 'ebook_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ebook_downloads');
+    }
+};
